dc37d0e7e956366e69035786d0139cfe0fd8ac1d
yvesgurcan <gurcan.yves+github@gmail.com>
2020-09-02 17:44:57 -0700

    Permission files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const app = require('express')();
-const server = require('http').createServer(app);
+
+const options = {
+    key: fs.readFileSync('./server-key.pem'),
+    cert: fs.readFileSync('./server-cert.pem')
+};
+
+const server = require('https').createServer(app);
 const io = require('socket.io')(server);
 const { PORT = 3000 } = process.env;
 
